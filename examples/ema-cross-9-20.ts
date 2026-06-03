3827d41173702bde6fb9fb032f021aa0f394fb8f
David Alsh <12656294+alshdavid@users.noreply.github.com>
2026-06-03 12:55:50 +1000

    Update ema-cross-9-20.ts

--- examples/ema-cross-9-20.ts
+++ examples/ema-cross-9-20.ts
@@ -1,3 +1,22 @@
-export async function handler(ctx) {
-  // TODO
+// TODO
+
+export default async function handler(ctx: IContext): Promise<void> {
+  // Stream that emits price data on each tick
+  ctx.onSpotEvent((event) => {
+    console.log({
+      symbol_id: event.symbol_id,   // Ticker
+      bid: event.bid,               // Buy price (includes spread)
+      ask: event.ask,               // Sell price (includes spread)
+      trendbar: event.trendbar,     // Can request candle information
+      timestamp: event.timestamp,   // Timestamp for tick event
+    })
+  })
+
+  const symbols = await ctx.symbolsList()
+
+  // This tells the broker we want to receive 
+  // updates for the supplied symbols
+  ctx.subscribeSpots({
+    symbols: [symbols.get('US500')!]
+  }) 
 }
