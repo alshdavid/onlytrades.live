09428eeed38b5fda12fb1328454f2f0d402b8d4a
David Alsh <alshdavid@gmail.com>
2026-06-04 13:02:05 +1000

    generate ctrader_proto

--- crates/kit/ctrader_proto/generate.bash
+++ crates/kit/ctrader_proto/generate.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # The generated files are committed because
 # the upstream code doesn't change much and
 # it's annoying to generate these types
@@ -9,7 +11,8 @@
 
 rm -rf ./lib.rs ./openapi-proto-messages
 
-curl -L https://github.com/spotware/openapi-proto-messages/archive/refs/heads/main.tar.gz | tar -xzf - -C ./openapi-proto-messages
+mkdir -p ./openapi-proto-messages
+curl -L https://github.com/spotware/openapi-proto-messages/archive/refs/heads/main.tar.gz | tar -xzf - --strip-components=1 -C ./openapi-proto-messages
 
 protoc --plugin=protoc-gen-prost=$(which protoc-gen-prost) \
   --proto_path=openapi-proto-messages \
